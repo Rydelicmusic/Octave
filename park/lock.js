ca2c41f1c6e25d44a59795e57899ff84d1ed4df0
Rydelicmusic <rrowe1995@gmail.com>
2026-09-20 17:58:51 -0500

    revert park lock barrel to 74cc53f modules

--- park/lock.js
+++ park/lock.js
@@ -1,3 +1,5 @@
 /** Restored 74cc53f lock surface. */
-export * from './lock-a.js';
+export * from './lock-a1.js';
+export * from './lock-a2.js';
 export * from './lock-b.js';
+export * from './lock-c.js';
